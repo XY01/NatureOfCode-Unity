692c9ca32c74d75ddc63fa8ab68e97c790408794
BradHammond <brad@exp.org.au>
2021-04-26 21:26:59 +1000

    Added example 2.5

--- Assets/The Nature of code/2 Forces/Scripts/Mover21.cs
+++ Assets/The Nature of code/2 Forces/Scripts/Mover21.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class Mover21 : MonoBehaviour 
 {
 	Vector3 		Location;
-	public Vector3 		Velocity;
+	public Vector3 	Velocity;
 	Vector3 		Acceleration;
 	public float 	Mass = 1;
 
+	//--- ADDED FOR TUTE 2.5
+	public bool		_InLiquidTrigger = false;
+	float			_LiquidDragCoefficient = .1f;
 
 
 	public void Initialize( float XPos, float YPos, float mass )
@@ -21,6 +24,9 @@ public class Mover21 : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		if (_InLiquidTrigger)
+			ApplyDrag(_LiquidDragCoefficient);
+
 		Velocity += Acceleration;
 		Location += Velocity * Time.deltaTime;	
 
@@ -35,7 +41,18 @@ public class Mover21 : MonoBehaviour
 		Acceleration += force;
 	}
 
-	public void CheckEdges( float areaWidth, float areaHeight ) 
+    void ApplyDrag(float dragCoefficient)
+    {
+		float speed = Velocity.magnitude;
+		float dragMagnitude = dragCoefficient * speed * speed;
+
+		Vector3 drag = -Velocity.normalized;
+		drag *= dragMagnitude;
+
+		ApplyForce(drag);
+	}
+
+    public void CheckEdges( float areaWidth, float areaHeight ) 
 	{		
 		//When it reaches one edge, set Location to the other.
 		if (Location.x > areaWidth/2f ) 
@@ -59,5 +76,23 @@ public class Mover21 : MonoBehaviour
 			Location.y = -areaHeight/2f;
 			Velocity.y = -Velocity.y;
 		}		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+		// 6 is the index of the 'Liquid trigger' layer
+        if(other.gameObject.layer == 6)
+        {
+			_InLiquidTrigger = true;
+		}
+    }
+
+	private void OnTriggerExit(Collider other)
+	{
+		// 6 is the index of the 'Liquid trigger' layer
+		if (other.gameObject.layer == 6)
+		{
+			_InLiquidTrigger = false;
+		}
 	}
 }
---
+++ Assets/The Nature of code/2 Forces/Scripts/NoC_2_5_FluidResistance.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class NoC_2_5_FluidResistance : MonoBehaviour
+{
+	public Vector2 	AreaSize = new Vector2( 5, 5 );
+
+	public Vector3 	WindForce = new Vector3 (1, 0 );
+	public Vector3 	Gravity = new Vector3 ( 0, -9.81f);
+
+	public Mover21 	MoverPrefab;
+	Mover21[]		Movers;
+
+	public int 		NumberOfMovers = 10;
+
+	public float	FrictionCoefficient = .1f;
+	float			FrictionNormal = 1;						// Assuming a normal of 1 for now as per the tute
+
+	// Use this for initialization
+	void Start ()
+	{
+		Movers = new Mover21[ NumberOfMovers ];
+
+		for (int i = 0; i < Movers.Length; i++)
+		{
+			Mover21 newMover = Instantiate( MoverPrefab ) as Mover21;
+			newMover.Initialize ( Random.Range(-AreaSize.x * .4f, AreaSize.x * .4f), AreaSize.y/2f, Random.Range( 1f, 4f ) );
+
+			Movers[i] = newMover;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float frictionMag = FrictionCoefficient * FrictionNormal;
+	
+
+		for (int i = 0; i < Movers.Length; i++) 
+		{
+			Vector3 friction = -(Movers[i].Velocity).normalized;
+			friction *= frictionMag;
+
+			Movers[i].ApplyForce(friction);
+			Movers[i].ApplyForce ( WindForce * Time.deltaTime);
+			Movers[i].ApplyForce ( Gravity * Movers[i].Mass * Time.deltaTime);
+			Movers[i].CheckEdges ( AreaSize.x, AreaSize.y );
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawWireCube (Vector3.zero, AreaSize);
+	}
+}
